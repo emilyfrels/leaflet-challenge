f02d4a9a3d27fd4475282eb3b1a096ba875c973b
emilyfrels <emily.n.frels@gmail.com>
2020-10-13 07:59:14 -0500

    map is displaying

---
+++ static/js/logic.js
@@ -0,0 +1,27 @@
+// console.log because we can
+console.log("logic.js for leaflet homework");
+
+// create map object
+var myMap = L.map("mapid", {
+    center: [ 36.114647, -115.172813],
+    zoom: 6
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Use this link to get the geojson data.
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Grabbing our GeoJSON data..
+d3.json(link).then(function(data) {
+  // Create a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(myMap);
+});
